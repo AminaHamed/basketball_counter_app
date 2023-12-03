61786774563962cd882bcb9f1d2d38ee8ca6a229
LAPTOP SHOP <aminahamed672000@gmail.com>
2023-12-03 18:04:38 +0200

    +ui

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:basketball_counter/home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const PointsCounter());
+}
+
+class PointsCounter extends StatelessWidget {
+  const PointsCounter({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    );
+  }
+}
